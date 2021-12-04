0d80c523b08af1e55f083ead45f62baea2e7b094
Jon Clayton <jon@arundo.com>
2021-12-04 02:39:23 -0600

    December 4

---
+++ december_5.py
@@ -0,0 +1,14 @@
+import utilities
+
+
+def first(string_input):
+    return 0
+
+
+def second(string_input):
+    return 0
+
+
+test_input = """asdf"""
+
+actual_input = """asdf"""
--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import utilities
 import december_1
 import december_2
 import december_3
+import december_4
+import december_5
 
 utilities.print_answer("December 1A", december_1.first(december_1.string_input))
 utilities.print_answer("December 1B", december_1.second(december_1.string_input))
+
 utilities.print_answer("December 2A", december_2.first(december_2.string_input))
 utilities.print_answer("December 2B", december_2.second(december_2.string_input))
 
 utilities.print_answer("December 3A", december_3.first(december_3.string_input))
 utilities.print_answer("December 3B", december_3.second(december_3.string_input))
+
+utilities.print_answer("December 4A test", december_4.first(december_4.test_input))
+utilities.print_answer("December 4A", december_4.first(december_4.actual_input))
+utilities.print_answer("December 4B test", december_4.second(december_4.test_input))
+utilities.print_answer("December 4B", december_4.second(december_4.actual_input))
+
+utilities.print_answer("December 5A test", december_5.first(december_5.test_input))
+utilities.print_answer("December 5A", december_5.first(december_5.actual_input))
+utilities.print_answer("December 5B test", december_5.second(december_5.test_input))
+utilities.print_answer("December 5B", december_5.second(december_5.actual_input))
