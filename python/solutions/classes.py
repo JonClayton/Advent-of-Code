89518004ae8fe8315b98614b4e03227114427d18
Jon Clayton <jon@arundo.com>
2021-12-07 12:59:45 -0600

    December 7 update

--- python/solutions/classes.py
+++ python/solutions/classes.py
@@ -7,7 +7,6 @@ class Solution:
         file_contents = open(file_path)
         data = json.load(file_contents)
         self.actual_lines = self._convert_to_array(data['actual_input'])
-        self.created_at = time_ns()
         self.day = data['day']
         self.first_solution = first_solution
         self.first_test_result = data['first_test_result']
@@ -17,6 +16,7 @@ class Solution:
         self.test_lines = self._convert_to_array(data['test_input'])
 
     def status_report(self):
+        started_at = time_ns()
         if self.day is None or self.first_test_result is None:
             return print(f'Must set day and/or first_test_result in constructor for December {self.day}')
         self.result = self.first_solution(self.test_lines)
@@ -28,7 +28,7 @@ class Solution:
             print(f'Solution for {self.day} part 1 is {first_result}')
             return self._report_failed_test(2, self.second_test_result)
         second_result = self.second_solution(self.actual_lines)
-        elapsed_time = round((time_ns() - self.created_at) / 1000000000, 3)
+        elapsed_time = round((time_ns() - started_at) / 1000000000, 3)
         print(f'December {self.day} solved in {elapsed_time}s: first = {first_result} and second = {second_result}')
 
     def _report_failed_test(self, part, expected_result):
