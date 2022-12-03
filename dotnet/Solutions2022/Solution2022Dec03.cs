b1e5a3171e1b027416ad8fa3aa4374416f3f1f08
Jon Clayton <jon@arundo.com>
2022-12-02 23:53:30 -0600

    One line solutions

--- dotnet/Solutions2022/Solution2022Dec03.cs
+++ dotnet/Solutions2022/Solution2022Dec03.cs
@@ -2,21 +2,11 @@ namespace AdventOfCode.Solutions2022;
 
 public class Solution2022Dec03 : Solution
 {
-    protected override long FirstSolution(List<string> lines)
-    {
-        return lines.Sum(line => GetCharValue(line.Take(line.Length / 2).ToHashSet()
-            .Intersect(line.TakeLast(line.Length / 2).ToHashSet()).First()));
-    }
+    protected override long FirstSolution(List<string> lines) => lines.Sum(line =>
+        GetCharValue(line.Take(line.Length / 2).Intersect(line.TakeLast(line.Length / 2)).First()));
 
-    protected override long SecondSolution(List<string> lines)
-    {
-        return lines.Chunk(3).Sum(chunk =>
-            GetCharValue(chunk[0].ToHashSet().Intersect(chunk[1].ToHashSet()).Intersect(chunk[2].ToHashSet()).First()));
-    }
+    protected override long SecondSolution(List<string> lines) => lines.Chunk(3)
+        .Sum(chunk => GetCharValue(chunk[0].Intersect(chunk[1]).Intersect(chunk[2]).First()));
 
-    private static long GetCharValue(char c)
-    {
-        var asciiValue = (int)c;
-        return asciiValue - (asciiValue > 96 ? 96 : 38);
-    }
+    private static long GetCharValue(char c) => c - (c > 96 ? 96 : 38);
 }
