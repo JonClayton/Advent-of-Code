29093721adc0b93dda816a15eea0fed17e41c66a
Jon Clayton <jon@arundo.com>
2022-12-08 02:57:44 -0600

    cleaned up solution

--- dotnet/Solutions2022/Solution2022Dec08.cs
+++ dotnet/Solutions2022/Solution2022Dec08.cs
@@ -2,59 +2,58 @@ namespace AdventOfCode.Solutions2022;
 
 public class Solution2022Dec08 : Solution
 {
-    protected override long FirstSolution(List<string> lines)
-    {
-        var trees = Tree.ReadTrees(lines);
-        return trees.Count(tree =>
-        {
-            var rowTrees = trees.Where(t => t.X.Equals(tree.X)).ToList();
-            if (!rowTrees.Any(t => t.Y < tree.Y && t.Height >= tree.Height)) return true;
-            if (!rowTrees.Any(t => t.Y > tree.Y && t.Height >= tree.Height)) return true;
-            var colTrees = trees.Where(t => t.Y.Equals(tree.Y)).ToList();
-            if (!colTrees.Any(t => t.X < tree.X && t.Height >= tree.Height)) return true;
-            return !colTrees.Any(t => t.X > tree.X && t.Height >= tree.Height);
-        });
-    }
-
-    protected override long SecondSolution(List<string> lines)
-    {
-        var trees = Tree.ReadTrees(lines);
-        var maxY = lines.Count - 1;
-        var maxX = lines.First().Length - 1;
-        return trees.Select(tree =>
-        {
-            var blockingTrees = trees
-                .Where(t => t.Height >= tree.Height && (t.X == tree.X || t.Y == tree.Y)).ToList();
-            var upY = (blockingTrees.Where(t => t.X == tree.X && t.Y > tree.Y).MinBy(t => t.Y)?.Y ?? maxY) - tree.Y;
-            var downY = tree.Y - (blockingTrees.Where(t => t.X == tree.X && t.Y < tree.Y).MaxBy(t => t.Y)?.Y ?? 0);
-            var upX = (blockingTrees.Where(t => t.Y == tree.Y && t.X > tree.X).MinBy(t => t.X)?.X ?? maxX) - tree.X;
-            var downX = tree.X - (blockingTrees.Where(t => t.Y == tree.Y && t.X < tree.X).MaxBy(t => t.X)?.X ?? 0);
-            return upX * upY * downX * downY;
-        }).Max();
-    }
+    protected override long FirstSolution(List<string> lines) => new Forest(lines).CountVisibleTrees();
+    protected override long SecondSolution(List<string> lines) => new Forest(lines).FindMaxScenicScore();
 }
-    public class Tree
+
+public class Forest
+{
+    public readonly Dictionary<(int, int), Tree> Trees = new();
+    public readonly int Height;
+    public readonly int Width;
+
+    public Forest(IReadOnlyList<string> lines)
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Height { get; set; }
-
-        public Tree(int x, int y, char h)
-        {
-            X = x;
-            Y = y;
-            Height = int.Parse(h.ToString());
-        }
-        
-        public static List<Tree> ReadTrees(List<string> lines) => lines.Aggregate((0, new List<Tree>()), (pair, line) =>
-        {
-            var (row, trees) = pair;
-            for (var col = 0; col < line.Length; col++)
-            {
-                trees.Add(new Tree(col, row, line[col]));
-            }
-
-            row++;
-            return (row, trees);
-        }).Item2;
+        Height = lines.Count;
+        Width = lines[0].Length;
+        for (var row = 0; row < Height; row++)
+        for (var col = 0; col < Width; col++)
+            Trees.Add((col, row), new Tree(col, row, lines[row][col], this));
     }
+
+    public int CountVisibleTrees() => Trees.Values.Count(tree => tree.CanBeSeenFromEdge());
+    public int FindMaxScenicScore() => Trees.Values.Select(tree => tree.ScenicScore()).Max();
+}
+
+public class Tree
+{
+    private readonly int _x;
+    private readonly int _y;
+    private readonly int _height;
+    private readonly Forest _forest;
+    
+    public Tree(int x, int y, char c, Forest forest)
+    {
+        _x = x;
+        _y = y;
+        _height = int.Parse(c.ToString());
+        _forest = forest;
+    }
+    
+    public bool CanBeSeenFromEdge() => Views.Any(view => _height > (view.MaxBy(t => t._height)?._height ?? -1));
+    public int ScenicScore() => Views.Aggregate(1, (product, view) =>
+    {
+        var countToNearestBlockingTree = view.FindIndex(t => t._height >= _height) + 1;
+        return product * (countToNearestBlockingTree == 0 ? view.Count : countToNearestBlockingTree);
+    });
+    
+    private List<Tree> DownView =>
+        Enumerable.Range(_y + 1, _forest.Height - _y - 1).Select(y => _forest.Trees[(_x, y)]).ToList();
+    private List<Tree> LeftView => 
+        _x == 0 ? new List<Tree>() : Enumerable.Range(0, _x).Reverse().Select(x => _forest.Trees[(x, _y)]).ToList();
+    private List<Tree> RightView =>
+        Enumerable.Range(_x + 1, _forest.Width - _x - 1).Select(x => _forest.Trees[(x, _y)]).ToList();
+    private List<Tree> UpView =>
+        _y == 0 ? new List<Tree>() : Enumerable.Range(0, _y).Reverse().Select(y => _forest.Trees[(_x, y)]).ToList();
+    private List<List<Tree>> Views => new() { DownView, LeftView, RightView, UpView };
+}
