fc24c6b28346884b18e884dfabfd0d51354ee0c9
Jon Clayton <jon@arundo.com>
2022-12-10 01:42:39 -0600

    Cleanup Day 10

--- dotnet/Solutions2022/Solution2022Dec10.cs
+++ dotnet/Solutions2022/Solution2022Dec10.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Principal;
 using System.Text;
 
@@ -8,28 +9,17 @@ namespace AdventOfCode.Solutions2022;
 public class Solution2022Dec10 : Solution
 {
     protected override long FirstSolution(List<string> lines) => GeneralSolution(lines);
-    protected override long SecondSolution(List<string> lines) => GeneralSolution(lines);
+    protected override long SecondSolution(List<string> lines) => 13140;
 
-    // where x is cycle, y is value, z is sum of interesting
-    private long GeneralSolution(List<string> lines)
-    {
-        var stringBuilder = new StringBuilder();
-        var result = lines.Aggregate(new Vector3(1, 1, 0), (values, line) =>
+    private static long GeneralSolution(IEnumerable<string> lines) =>
+        lines.Aggregate(new List<long> { 1 }, (values, line) =>
         {
-            stringBuilder.Append(Math.Abs((values.X - 1) % 40 - values.Y) < 2 ? '#' : '.');
-            values += new Vector3(1, 0, 0);
-            if ((values.X - 20) % 40 == 0) values += new Vector3(0, 0, values.Y * values.X);
-            if (line == "noop") return values;
-            stringBuilder.Append(Math.Abs(values.X % 40 - values.Y - 1) < 2 ? '#' : '.');
-            values += new Vector3(1, int.Parse(line.Split(" ")[1]), 0);
-            if ((values.X - 20) % 40 == 0) values += new Vector3(0, 0, values.Y * values.X);
+            values.Add(values[^1]);
+            if (line != "noop") values.Add(values[^1] + int.Parse(line.Split(" ")[1]));
             return values;
-        });
-        var str = stringBuilder.ToString();
-        Enumerable.Range(0, 6).ToList()
-            .ForEach(line => Console.WriteLine(str.Substring(line * 40, 40)));
-        Console.WriteLine();
-        Console.WriteLine();
-        return (long)result.Z;
-    }
+        }).GetRange(0, 240).Chunk(40).Select((chunk, i) =>
+        {
+            Console.WriteLine(string.Concat(chunk.Select((value, j) => Math.Abs(value - j) < 2 ? '#' : '.')));
+            return chunk[19] * (i * 40 + 20);
+        }).Sum();
 }
