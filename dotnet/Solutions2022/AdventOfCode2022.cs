172db3363e692a648e7bd7f667af5c8c50b9dd6e
Jon Clayton <jon@arundo.com>
2022-12-10 15:24:41 -0600

    working Dec 9/2

--- dotnet/Solutions2022/AdventOfCode2022.cs
+++ dotnet/Solutions2022/AdventOfCode2022.cs
@@ -4,15 +4,15 @@ public static class AdventOfCode2022
 {
     public static void Solve()
     {
-        new Solution2022Dec01().StatusReport();
-        new Solution2022Dec02().StatusReport();
-        new Solution2022Dec03().StatusReport();
-        new Solution2022Dec04().StatusReport();
-        new Solution2022Dec05().StatusReport();
-        new Solution2022Dec06().StatusReport();
-        new Solution2022Dec07().StatusReport();
-        new Solution2022Dec08().StatusReport();
-        // new Solution2022Dec09().StatusReport();
-        new Solution2022Dec10().StatusReport();
+        // new Solution2022Dec01().StatusReport();
+        // new Solution2022Dec02().StatusReport();
+        // new Solution2022Dec03().StatusReport();
+        // new Solution2022Dec04().StatusReport();
+        // new Solution2022Dec05().StatusReport();
+        // new Solution2022Dec06().StatusReport();
+        // new Solution2022Dec07().StatusReport();
+        // new Solution2022Dec08().StatusReport();
+        new Solution2022Dec09().StatusReport();
+        // new Solution2022Dec10().StatusReport();
     }
 }
--- dotnet/Solutions2022/Solution2022Dec09.cs
+++ dotnet/Solutions2022/Solution2022Dec09.cs
@@ -1,30 +1,37 @@
 using System.Numerics;
-using System.Security.Principal;
 
 namespace AdventOfCode.Solutions2022;
 
 public class Solution2022Dec09 : Solution
 {
-    private Dictionary<char, Vector2> Moves = new Dictionary<char, Vector2>()
+    private readonly Dictionary<char, Vector2> _moves = new Dictionary<char, Vector2>()
     {
         {'D', new Vector2(0,-1)},
         {'L', new Vector2(-1,0)},
         {'R', new Vector2(1,0)},
         {'U', new Vector2(0,1)},
     };
+    
+    private readonly Vector2 _negativeOne = new Vector2(-1, -1);
+    private readonly Vector2 _positiveOne = new Vector2(1, 1);
 
     protected override long FirstSolution(List<string> lines) => GeneralSolution(lines, 2);
     protected override long SecondSolution(List<string> lines) => GeneralSolution(lines, 10);
     
     private long GeneralSolution(List<string> lines, int knotCount)
     {
-        var negativeOne = new Vector2(-1, -1);
-        var positiveOne = new Vector2(1, 1);
+
         var knots = Enumerable.Range(0, knotCount).Select(i => new Vector2(0, 0)).ToList();
         var tailPositions = new HashSet<Vector2> { knots.Last() };
-        lines.ForEach(line =>
+        lines.SelectMany(line =>
         {
-            var move = Moves[line[0]] * int.Parse(line.Split(" ")[1]);
+            {
+                var moveCountArray = Enumerable.Range(0, int.Parse(line.Split(" ")[1])); 
+                var result = moveCountArray.Select(i => _moves[line[0]]);
+                return result;
+            }
+        }).ToList().ForEach(move => 
+        {
             for (var i = 0; i < knotCount - 1; i++)
             {
                 knots[i] = Vector2.Add(knots[i], move);
@@ -32,20 +39,20 @@ public class Solution2022Dec09 : Solution
                 var nextKnotMove = new Vector2(0, 0);
                 while (nextKnotRelativePosition.Length() >= 2)
                 {
-                    var incrementalMove = Vector2.Clamp(nextKnotRelativePosition, negativeOne, positiveOne);
+                    var incrementalMove = Vector2.Clamp(nextKnotRelativePosition, _negativeOne, _positiveOne);
                     nextKnotMove = Vector2.Add(nextKnotMove, incrementalMove);
                     nextKnotRelativePosition = Vector2.Add(nextKnotRelativePosition, -incrementalMove);
                     if (i == knotCount - 2)
                     {
                         tailPositions.Add(knots[i] - nextKnotRelativePosition);
-                       if (knotCount > 3) Console.WriteLine($"Last: {knots[i] - nextKnotRelativePosition}");
+                       // if (knotCount > 3) Console.WriteLine($"Last: {knots[i] - nextKnotRelativePosition}");
                     }
                 }
 
                 move = nextKnotMove;
             }
-
             knots[^1] += move;
+
         });
         return tailPositions.Count;
     }
