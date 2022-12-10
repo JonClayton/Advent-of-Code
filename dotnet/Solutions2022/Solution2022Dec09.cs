fdab00dccaa27c7b2bdac1ee93ff243cda02a896
Jon Clayton <jon@arundo.com>
2022-12-10 16:10:30 -0600

    Clean up 12/9 solution

--- dotnet/Solutions2022/Solution2022Dec09.cs
+++ dotnet/Solutions2022/Solution2022Dec09.cs
@@ -4,56 +4,40 @@ namespace AdventOfCode.Solutions2022;
 
 public class Solution2022Dec09 : Solution
 {
-    private readonly Dictionary<char, Vector2> _moves = new Dictionary<char, Vector2>()
+    private readonly Dictionary<char, Vector2> _directions = new()
     {
-        {'D', new Vector2(0,-1)},
-        {'L', new Vector2(-1,0)},
-        {'R', new Vector2(1,0)},
-        {'U', new Vector2(0,1)},
+        { 'D', -Vector2.UnitY },
+        { 'L', -Vector2.UnitX },
+        { 'R', Vector2.UnitX },
+        { 'U', Vector2.UnitY },
     };
-    
-    private readonly Vector2 _negativeOne = new Vector2(-1, -1);
-    private readonly Vector2 _positiveOne = new Vector2(1, 1);
 
     protected override long FirstSolution(List<string> lines) => GeneralSolution(lines, 2);
     protected override long SecondSolution(List<string> lines) => GeneralSolution(lines, 10);
-    
-    private long GeneralSolution(List<string> lines, int knotCount)
+
+    private long GeneralSolution(IEnumerable<string> lines, int knotCount)
     {
-
-        var knots = Enumerable.Range(0, knotCount).Select(i => new Vector2(0, 0)).ToList();
-        var tailPositions = new HashSet<Vector2> { knots.Last() };
-        lines.SelectMany(line =>
-        {
+        return lines.Aggregate((Enumerable.Range(0, knotCount).Select(i => Vector2.Zero).ToList(),
+                new HashSet<Vector2> { Vector2.Zero }),
+            (pair, line) =>
             {
-                var moveCountArray = Enumerable.Range(0, int.Parse(line.Split(" ")[1])); 
-                var result = moveCountArray.Select(i => _moves[line[0]]);
-                return result;
-            }
-        }).ToList().ForEach(move => 
-        {
-            for (var i = 0; i < knotCount - 1; i++)
-            {
-                knots[i] = Vector2.Add(knots[i], move);
-                var nextKnotRelativePosition = knots[i] - knots[i + 1];
-                var nextKnotMove = new Vector2(0, 0);
-                while (nextKnotRelativePosition.Length() >= 2)
+                var (currentKnotLocations, cabooseLocations) = pair;
+                var direction = _directions[line[0]];
+                var distance = int.Parse(line.Split(" ")[1]);
+                Enumerable.Range(0, distance).Select(i => direction).ToList().ForEach(move =>
                 {
-                    var incrementalMove = Vector2.Clamp(nextKnotRelativePosition, _negativeOne, _positiveOne);
-                    nextKnotMove = Vector2.Add(nextKnotMove, incrementalMove);
-                    nextKnotRelativePosition = Vector2.Add(nextKnotRelativePosition, -incrementalMove);
-                    if (i == knotCount - 2)
+                    currentKnotLocations = currentKnotLocations.Select((knot, i) =>
                     {
-                        tailPositions.Add(knots[i] - nextKnotRelativePosition);
-                       // if (knotCount > 3) Console.WriteLine($"Last: {knots[i] - nextKnotRelativePosition}");
-                    }
-                }
-
-                move = nextKnotMove;
-            }
-            knots[^1] += move;
-
-        });
-        return tailPositions.Count;
+                        knot += move;
+                        if (i < knotCount - 1)
+                            move = (knot - currentKnotLocations[i + 1]).Length() >= 2
+                                ? Vector2.Clamp(knot - currentKnotLocations[i + 1], -Vector2.One, Vector2.One)
+                                : Vector2.Zero;
+                        else cabooseLocations.Add(knot);
+                        return knot;
+                    }).ToList();
+                });
+                return (currentKnotLocations, cabooseLocations);
+            }).Item2.Count;
     }
 }
